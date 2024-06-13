267446e4af3c9b0773d364a1d53973f4df211ce8
Nikita Komarov <privet224000@mail.ru>
2024-06-14 02:21:01 +0300

    Create a new branch with only PHP_API folder

--- GOL_WEB/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// index.php
-header('Location: Registration_page/templates/registration.html');
-// header('Location: main_list/templates/main_list.php');
-exit;
-?>
--- GOL_WEB/test/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('hintButton').addEventListener('click', function() {
-    var hint = document.getElementById('hint');
-    if (hint.classList.contains('hidden')) {
-        hint.classList.remove('hidden');
-    } else {
-        hint.classList.add('hidden');
-    }
-});
