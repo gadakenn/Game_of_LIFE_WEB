427c7288bd973219fae1ae14de4771e12e0d505f
Nikita Komarov <112871630+gadakenn@users.noreply.github.com>
2024-06-14 11:55:53 +0300

    change private domain flask

--- GOL_WEB/Game/get_game_data.php
+++ GOL_WEB/Game/get_game_data.php
@@ -90,7 +90,7 @@ function updateBalanceDB($gameId, $currentRoundId) {
 }
 
 function call_chatgpt($data) {
-    $url = 'http://game_of_life_web.railway.internal:8000/run_chatgpt';
+    $url = 'http://divine-growth.railway.internal:8000/run_chatgpt';
 
     $options = array(
         'http' => array(
