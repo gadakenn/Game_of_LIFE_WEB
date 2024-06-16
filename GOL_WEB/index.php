87002d05a24cf6c1dcafb4240d995128778ca3ad
Nikita Komarov <112871630+gadakenn@users.noreply.github.com>
2024-06-17 02:59:23 +0300

    enter logic updated

--- GOL_WEB/index.php
+++ GOL_WEB/index.php
@@ -1,6 +1,11 @@
 <?php
 // index.php
-header('Location: Registration_page/templates/registration.html');
-// header('Location: main_list/templates/main_list.php');
+if (isset($_SESSION['user'])) {
+    header('Location: main_list/templates/main_list.php');
+} else {
+    header('Location: Registration_page/templates/registration.html');
+}
+
+
 exit;
 ?>
